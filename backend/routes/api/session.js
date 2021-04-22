adc3571ef172074ae32f2b5348750c683f1dbc8b
earlwoo <earl.woo@gmail.com>
2021-04-22 13:15:28 -0400

    added login route

---
+++ backend/routes/api/session.js
@@ -0,0 +1,33 @@
+// backend/routes/api/session.js
+// backend/routes/api/session.js
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const router = express.Router();
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+router.post(
+    '/',
+    asyncHandler(async (req, res, next) => {
+      const { credential, password } = req.body;
+
+      const user = await User.login({ credential, password });
+
+      if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['The provided credentials were invalid.'];
+        return next(err);
+      }
+
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user,
+      });
+    }),
+  );
+
+  module.exports = router;
