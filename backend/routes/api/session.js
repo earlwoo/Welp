04dbdd8ccb0ae99de55397a39500484447e74be7
earlwoo <earl.woo@gmail.com>
2021-04-22 13:18:39 -0400

    logout route done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,27 +7,30 @@ const router = express.Router();
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
-router.post(
-    '/',
-    asyncHandler(async (req, res, next) => {
-      const { credential, password } = req.body;
+router.post('/', asyncHandler(async (req, res, next) => {
+        const { credential, password } = req.body;
 
-      const user = await User.login({ credential, password });
+        const user = await User.login({ credential, password });
 
-      if (!user) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = ['The provided credentials were invalid.'];
-        return next(err);
-      }
+        if (!user) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials were invalid.'];
+            return next(err);
+        }
 
-      await setTokenCookie(res, user);
+        await setTokenCookie(res, user);
 
-      return res.json({
-        user,
-      });
+        return res.json({
+            user,
+        });
     }),
-  );
+);
 
-  module.exports = router;
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+});
+
+module.exports = router;
