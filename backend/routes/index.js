44f1e47e9e4ba7447976ff2cbf0af33fa846a8aa
earlwoo <earl.woo@gmail.com>
2021-04-23 16:04:32 -0400

    added heroku json

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -36,10 +36,10 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+// router.get('/hello/world', function (req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
 
 
 
